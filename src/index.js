0c9e2427413becb1b8a7fc655d06abedb1bc4f06
anton-ua <antonyua23@gmail.com>
2020-06-13 14:16:07 +0300

    fix routing

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { PersistGate } from "redux-persist/integration/react";
 ReactDOM.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter>
+      <BrowserRouter basename="/goit-react-hw-08-phonebook">
         <React.StrictMode>
           <App />
         </React.StrictMode>
